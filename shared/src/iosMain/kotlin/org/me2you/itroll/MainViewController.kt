1ac15a8127716f03c2ed98f9d036fced30ba3c99
Bidanski <bidanski666@gmail.com>
2026-08-05 10:39:56 +0300

    ...

--- shared/src/iosMain/kotlin/org/me2you/itroll/MainViewController.kt
+++ shared/src/iosMain/kotlin/org/me2you/itroll/MainViewController.kt
@@ -1,12 +1,19 @@
 package org.me2you.itroll
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.koin.compose.koinInject
+import org.me2you.itroll.ui.navigation.Root
 import org.me2you.itroll.ui.navigation.RootNavigation
+import org.me2you.itroll.ui.navigation.RootNavigator
 import org.me2you.itroll.ui.theme.iTrollTheme
 
 fun MainViewController() = ComposeUIViewController {
     iTrollTheme {
+        val rootNavigator: RootNavigator = koinInject<RootNavigator>()
 
-        RootNavigation()
+        RootNavigation(
+            rootNavigator = rootNavigator,
+            startRoute = Root
+        )
     }
 }
